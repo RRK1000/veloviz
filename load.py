7adddb3dbca45f6e2f8c9d251adacc5ae5e2776f
Rishi Ravikumar <rishiravi.k98@gmail.com>
2025-02-02 07:41:30 -0500

    feat: update load function to add json
    Signed-off-by: Rishi Ravikumar <rishiravi.k98@gmail.com>

--- load.py
+++ load.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from pymongo import MongoClient
 
 def insert_csv_to_mongodb(csv_file, mongo_config, db_name, collection_name):
@@ -49,8 +50,33 @@ def insert_csv_to_mongodb(csv_file, mongo_config, db_name, collection_name):
         # Close the MongoDB connection
         client.close()
 
-# traffic signals data
-csv_file = 'traffic-signals-timing.csv'
+def insert_json_to_mongodb(json_file, mongo_config, db_name, collection_name):
+    """Writes data from a JSON file into a MongoDB collection (list of JSONs)"""
+    try:
+        # Create MongoDB client
+        client = MongoClient(mongo_config['host'], mongo_config['port'], 
+                            username=mongo_config.get('username'), 
+                            password=mongo_config.get('password'), 
+                            authSource=mongo_config.get('authSource'))
+        
+        # Connect to the database and collection
+        db = client[db_name]
+        collection = db[collection_name]
+        
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                collection.insert_many(data)
+            else:
+                collection.insert_one(data)
+        
+        print(f"Data from '{json_file}' written to collection '{collection_name}' in database '{db_name}'")
+    except Exception as e:
+        print(f"An error occurred while writing to the collection: {e}")
+
+
+# csv data
+csv_file = 'cycling-network-4326.csv'
 mongo_config = {
     "host": "localhost",
     "port": 27017,
@@ -59,20 +85,11 @@ mongo_config = {
     "authSource": "admin"
 }
 db_name = 'fireball'
-collection_name = 'traffic-signals-timing'
+collection_name = 'cycling-network'
 
 insert_csv_to_mongodb(csv_file, mongo_config, db_name, collection_name)
 
-# traffic volumes data
-csv_file = 'svc_most_recent_summary_data.csv'
-mongo_config = {
-    "host": "localhost",
-    "port": 27017,
-    "username": "mongoadmin",
-    "password": "mongoadmin",
-    "authSource": "admin"
-}
-db_name = 'fireball'
-collection_name = 'traffic-volumes'
-
-insert_csv_to_mongodb(csv_file, mongo_config, db_name, collection_name)
+# json data
+json_file = 'station_data.json'
+collection_name = 'station-data'
+insert_json_to_mongodb(json_file, mongo_config, db_name, collection_name)
